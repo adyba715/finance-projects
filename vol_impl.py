702e048fbdc257066cc2b5a0203baaf7ce5a68a6
adyba715 <baady64@gmail.com>
2026-06-22 17:26:03 +0200

    Create vol_impl.py

---
+++ vol_impl.py
@@ -0,0 +1,21 @@
+%%writefile vol_impl.py
+import numpy as np
+
+from bs_pricer import Option
+
+def vol_implicite(Cmarche,S,K,r,T,type_option):
+  sigma=0.20
+  epsilon=1e-6
+  opt=Option(S,K,r,T,sigma,type_option)
+  prixBS=opt.prix()
+
+  while np.absolute(prixBS-Cmarche)>epsilon:
+    opt=Option(S,K,r,T,sigma,type_option)
+    prixBS=opt.prix()
+    vega=opt.vega()*100
+    sigma=sigma-(prixBS-Cmarche)/vega 
+
+   
+    
+
+  return sigma
