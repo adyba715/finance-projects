9d4fdb318e776ed9cfaceeb8e85bfb9962617b30
adyba715 <baady64@gmail.com>
2026-06-23 14:50:47 +0200

    Update vol_impl.py

--- project-1-bs-pricer/vol_impl.py
+++ project-1-bs-pricer/vol_impl.py
@@ -1,6 +1,6 @@
 import numpy as np
 from bs_pricer import Option
-def vol_implicite(Cmarche, S, K, r, T, type_option, max_iter=100, epsilon=1e-6):
+def vol_implicite(Cmarche, S, K, r, T, type_option, max_iter=1000, epsilon=1e-6):
     sigma = 0.20
     
     for i in range(max_iter):
