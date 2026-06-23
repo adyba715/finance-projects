b8960eecfa79f3912301ce631c6abcfcf902a4f8
adyba715 <baady64@gmail.com>
2026-06-23 14:55:35 +0200

    Update vol_impl.py

--- project-1-bs-pricer/vol_impl.py
+++ project-1-bs-pricer/vol_impl.py
@@ -1,20 +1,16 @@
 import numpy as np
 from bs_pricer import Option
-def vol_implicite(Cmarche, S, K, r, T, type_option, max_iter=1000, epsilon=1e-6):
+def vol_implicite(Cmarche, S, K, r, T, type_option):
     sigma = 0.20
+    epsilon = 1e-6
+    opt = Option(S, K, r, T, sigma, type_option)
+    prixBS = opt.prix()
     
-    for i in range(max_iter):
+    while np.abs(prixBS - Cmarche) > epsilon:
         opt = Option(S, K, r, T, sigma, type_option)
         prixBS = opt.prix()
         vega = opt.vega() * 100
-        
-        if np.abs(prixBS - Cmarche) < epsilon:
-            return sigma
-        
-        if vega < 1e-8:
-            return np.nan
-        
         sigma = sigma - (prixBS - Cmarche) / vega
-        sigma = max(sigma, 1e-4)
     
-    return np.nan
+    return sigma
+   
